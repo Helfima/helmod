35c39dc80a9d2c4a26f579ef568de2a14371ccc1
Helfima <helfima-game@wanadoo.fr>
2020-02-16 23:45:51 +0100

    fixed iteration end on bluiding user cache
        - fixed iteration end on bluiding user cache
        - fixed filter on recipe selector

--- test_find.lua
+++ test_find.lua
@@ -1,17 +1,2 @@
-local operation = 10
-
-function numberToBinStr(x)
-  ret=""
-  while x~=1 and x~=0 do
-    ret=tostring(x%2)..ret
-    x=math.modf(x/2)
-  end
-  ret=tostring(x)..ret
-  return ret
-end
-
-print(string.format("%s",numberToBinStr(operation)))
-
-local v1,v2,v3 = string.match("0.9.3", "([0-9]+)[.]([0-9]+)[.]([0-9]+)")
-
-print(string.format("%s",numberToBinStr(operation)))
+local value = string.find("pure-natural-gas","pure-natural-gas",1,true)
+print(value)
