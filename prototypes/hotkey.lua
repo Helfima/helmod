22a3af6b5ae67da2c4b110af1a8acfb7606f377b
Leonard Brünings <lord_damokles@gmx.net>
2020-05-01 10:54:19 +0200

    Fix deprecated custom-input for 0.18.22

--- prototypes/hotkey.lua
+++ prototypes/hotkey.lua
@@ -4,35 +4,35 @@ data:extend(
       type = "custom-input",
       name = "helmod-close",
       key_sequence = "ESCAPE",
-      consuming = "script-only"
+      consuming = "none"
 
     },
     {
       type = "custom-input",
       name = "helmod-open-close",
       key_sequence = "U",
-      consuming = "script-only"
+      consuming = "none"
 
     },
     {
       type = "custom-input",
       name = "helmod-production-line-open",
       key_sequence = "I",
-      consuming = "script-only"
+      consuming = "none"
 
     },
     {
       type = "custom-input",
       name = "helmod-recipe-selector-open",
       key_sequence = "O",
-      consuming = "script-only"
+      consuming = "none"
 
     },
     {
       type = "custom-input",
       name = "helmod-recipe-explorer-open",
       key_sequence = "Y",
-      consuming = "script-only"
+      consuming = "none"
 
     }
   }
