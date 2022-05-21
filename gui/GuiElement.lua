7719cd9ea5e33840c711aaeb785efe5925b382c5
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2022-05-21 18:34:48 +1200

    Format fluid temperature #394
    Format large fluid temperature values. e.g. 10k degrees

--- gui/GuiElement.lua
+++ gui/GuiElement.lua
@@ -287,7 +287,7 @@ function GuiElement.infoRecipe(parent, element)
     tooltip = {"tooltip.energy-recipe"}
   elseif element.type == "boiler" then
     local style = "helmod_temperature_blue_m"
-    local caption = {"", element.output_fluid_temperature, "°"}
+    local caption = Format.formatNumberKilo(element.output_fluid_temperature, "°")
     local label = GuiElement.add(parent, GuiLabel("temperature"):caption(caption):style(style):ignored_by_interaction())
     label.style.top_padding = -5
   elseif element.type ~= "recipe" then
