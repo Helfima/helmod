46fe9cce06b62c6ab5144432f3d49edfbaf45ad7
Helfima <helfima-game@wanadoo.fr>
2024-11-17 03:15:53 +0100

    removed information in title (Quality not implemented)

--- dialog/ProductionPanel.lua
+++ dialog/ProductionPanel.lua
@@ -10,7 +10,7 @@ end)
 -------------------------------------------------------------------------------
 ---On initialization
 function ProductionPanel:onInit()
-	self.panelCaption = string.format("%s %s %s", "Helmod", script.active_mods["helmod"], "Experimental (Quality not implemented)")
+	self.panelCaption = string.format("%s %s %s", "Helmod", script.active_mods["helmod"], "Experimental")
 end
 
 -------------------------------------------------------------------------------
